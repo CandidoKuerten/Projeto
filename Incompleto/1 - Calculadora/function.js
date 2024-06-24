d96d99a3e371dd0e0b74f620c2032e4d3dbc7804
CandidoKuerten <166192534+CandidoKuerten@users.noreply.github.com>
2024-06-24 12:19:40 -0300

    Criação - Projeto Calculadora

---
+++ Incompleto/1 - Calculadora/function.js
@@ -0,0 +1,20 @@
+function calcular(){
+    let operacao = ''
+    switch(operacao){
+        case '+':
+
+        break
+        case '-':
+
+        break
+        case '/':
+
+        break
+        case '*':
+
+        break
+    }
+}
+function limpar(){
+    
+}
