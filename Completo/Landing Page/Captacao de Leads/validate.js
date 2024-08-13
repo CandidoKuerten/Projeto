b347fca93f75a8fd62b84be3af6930326d5af9f5
CandidoKuerten <166192534+CandidoKuerten@users.noreply.github.com>
2024-08-13 16:07:41 -0300

    Projeto Landing Page

---
+++ Completo/Landing Page/Captacao de Leads/validate.js
@@ -0,0 +1,44 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const x = document.querySelector("#correcao");
+    let mensagem = "";
+
+    function validateNumber() {
+        console.log('validado');
+        if (parseInt(x.value) === 10) {
+            return true;
+        } else {
+            mensagem = "Valor incorreto!";
+            return false;
+        }
+    }
+
+    function validateForm(event) {
+        event.preventDefault();
+        if (!validateNumber()) {
+            event.preventDefault()
+        }
+        document.querySelector("#mensagem").innerHTML = mensagem;
+        openModal()
+        setTimeout(() =>{
+            location.reload()
+        }, 5000)
+    }
+
+    document.querySelector("form").addEventListener("submit", validateForm);
+
+    const modal = document.querySelector('.modal');
+    const conteudo = document.querySelector('.conteudo');
+
+    function openModal() {
+        modal.classList.add('active');
+        conteudo.classList.add('opacidade');
+    }
+
+    function closeModal() {
+        modal.classList.remove('active');
+        conteudo.classList.remove('opacidade');
+    }
+
+    window.openModal = openModal;
+    window.closeModal = closeModal;
+});
